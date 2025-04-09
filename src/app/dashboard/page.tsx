59efdc13acf1e6b4940beafbb77ba498997ab477
MacroHEX <martin18py@gmail.com>
2025-04-09 12:16:31 -0300

    feat: Se actualiza estilos DashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
-import {Lexend as FontSans, Manrope as FontManrope, Newsreader as FontSerif,} from "next/font/google"
+import {Lexend as FontSans, Manrope as FontManrope, Newsreader as FontSerif} from "next/font/google";
 
-import {cn} from "@/lib/utils"
+import {cn} from "@/lib/utils";
 import DashboardScreen from "@/app/screens/DashboardScreen";
 
-const fontSans = FontSans({subsets: ["latin"], variable: "--font-sans"})
-const fontSerif = FontSerif({subsets: ["latin"], variable: "--font-serif"})
+const fontSans = FontSans({subsets: ["latin"], variable: "--font-sans"});
+const fontSerif = FontSerif({subsets: ["latin"], variable: "--font-serif"});
 const fontManrope = FontManrope({
   subsets: ["latin"],
   variable: "--font-manrope",
-})
+});
 
 export default function DashboardPage() {
   return (
@@ -20,9 +20,7 @@ export default function DashboardPage() {
         fontManrope.variable
       )}
     >
-      <div className="w-full max-w-sm md:max-w-3xl">
-        <DashboardScreen/>
-      </div>
+      <DashboardScreen/>
     </div>
   );
 }

3038774141cd7347bf0163bd7fdc6828be05fa15
macrohex <martin18py@gmail.com>
2025-04-23 15:57:13 -0300

    feat: Se agrega pexel

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         hostname: 'images.unsplash.com'
+      },
+      {
+        hostname: 'images.pexels.com'
       }
     ]
   }
